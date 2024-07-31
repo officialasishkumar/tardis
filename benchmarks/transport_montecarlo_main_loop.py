9255013d50ec50faf7eef595e12ffc335b7edb0b
Asish Kumar <87874775+officialasishkumar@users.noreply.github.com>
2024-07-31 23:19:01 +0530

    Cache benchmark functions (#76)
    * added setup function
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * refactor geometry calculate distances
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * Refactor montecarlo estimators in benchmark code
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * add cache and setup functions
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * add cache and setup functions
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * ran ruff linter
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * Thou hath vanquished the redundant variables and functions
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * fix bugs
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * change benchmark yml
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * repeat in class
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * rounds in benchmarks
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    ---------
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- benchmarks/transport_montecarlo_main_loop.py
+++ benchmarks/transport_montecarlo_main_loop.py
@@ -13,14 +13,25 @@ class BenchmarkTransportMontecarloMontecarloMainLoop(BenchmarkBase):
     class to benchmark montecarlo_main_loop function.
     """
 
+    repeat = 3
+
+    def setup(self):
+        self.packet_collection = self.transport_state.packet_collection
+        self.geometry_state = self.transport_state.geometry_state
+        self.time_explosion = self.verysimple_time_explosion
+        self.opacity_state = self.transport_state.opacity_state
+        self.radfield_mc_estimators = (
+            self.transport_state.radfield_mc_estimators
+        )
+
     def time_montecarlo_main_loop(self):
         montecarlo_main_loop(
-            self.transport_state.packet_collection,
-            self.transport_state.geometry_state,
-            self.verysimple_time_explosion,
-            self.transport_state.opacity_state,
+            self.packet_collection,
+            self.geometry_state,
+            self.time_explosion,
+            self.opacity_state,
             self.montecarlo_configuration,
-            self.transport_state.radfield_mc_estimators,
+            self.radfield_mc_estimators,
             self.nb_simulation_verysimple.transport.spectrum_frequency_grid.value,
             self.rpacket_tracker_list,
             self.montecarlo_configuration.NUMBER_OF_VPACKETS,
